8d07ece7663da1a24185988975df8feba4e4d45a
Sophia Allen <smlelevich@gmail.com>
2016-04-26 10:50:51 -0700

    initial commit: uploaded files

---
+++ sampleservice/App_Code/BookReviewSampleService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BookReviewSampleService" in code, svc and config file together.
+//Note: the service implements the methods that are contracted in the interface. 
+public class BookReviewSampleService : IBookReviewSampleService
+{
+    BookReviewDbEntities dbe = new BookReviewDbEntities();
+
+    public List<string> GetAuthors()
+    {
+        var auth = from a in dbe.Authors
+                   orderby a.AuthorName
+                   select new { a.AuthorName };
+        List<string> authors = new List<string>();
+        foreach(var au in auth)
+        {
+            authors.Add(au.AuthorName.ToString());
+        }
+
+        return authors;
+    }
+
+    public List<BookLite> GetBooks(string authorName)
+    {
+        var bks = from b in dbe.Books
+                  from a in b.Authors
+                  orderby b.BookTitle
+                  where a.AuthorName.Equals(authorName)
+                  select new
+                  {
+                      b.BookTitle,
+                      a.AuthorName,
+                      b.BookISBN
+                  };
+        List<BookLite> books = new List<BookLite>();
+        foreach(var bk in bks)
+        {
+            BookLite bl = new BookLite();
+            bl.Title = bk.BookTitle;
+            bl.AuthorName = bk.AuthorName;
+            books.Add(bl);
+        }
+        return books;
+    }
+}
---
+++ sampleservice/App_Code/IBookReviewSampleService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBookReviewSampleService" in both code and config file together.
+[ServiceContract]
+public interface IBookReviewSampleService
+{
+    [OperationContract]
+    List<string> GetAuthors();
+
+    [OperationContract]
+    List<BookLite> GetBooks(string authorName);
+}
+
+
+[DataContract]
+public class BookLite
+{
+    [DataMember] 
+    public string Title { set; get; }
+
+    [DataMember] 
+    public string ISBN { set; get; }
+
+    [DataMember]
+    public string AuthorName { set; get; }
+}
